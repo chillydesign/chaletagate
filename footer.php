a5a60cf5dceadbe0432a551812ee6d18e3766dc9
Charles Harvey <harvey.charles@gmail.com>
2018-02-20 12:25:59 +0100

    initialised repo

---
+++ footer.php
@@ -0,0 +1,72 @@
+
+	<?php $tdu = get_template_directory_uri(); ?>
+
+    <footer>
+        <div class="container">
+
+            <div class="row">
+
+
+
+                <div class="col-sm-6 col-md-9">
+
+                    <div class="row">
+
+                        <div class="col-md-6" id="ensr_col">
+                            <img src="<?php echo $tdu; ?>/img/ensr_blue.jpg"  alt="">
+                            <p >Chemin de Rovéréaz 20 <br> Case postale 161 <br> CH–1000 Lausanne 12 <br> Switzerland <br> <a href="http://www.ensr.ch">www.ensr.ch</a></p>
+
+
+                        </div>
+                        <div class="col-md-6" id="summercamp_col">
+                            <img src="<?php echo $tdu; ?>/img/summer_camp_blue.jpg"  alt="">
+                            <p>Hôtel Suisse  <br> Champéry, Valais <br> Switzerland  <br> <a href="http://www.ensr-summer.ch">www.ensr-summer.ch </a>    </p>
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+                <div class="col-sm-6 col-md-3" id="ensr_summer_col">
+									<h2>Contactez-nous</h2>
+									<p>Tel: +41 21 654 65 00<br> Email : <a href="mailto:info@ensr.ch">info@ensr.ch</a></p>
+									<div class="social_media">
+										<a href="https://www.facebook.com/EcoleNouvelleENSR/" class="social_icon" id="facebook" target="blank">Facebook</a>
+										<a href="https://www.instagram.com/ensrlife/" class="social_icon" id="instagram" target="blank">Instagram</a>
+										<a href="https://www.youtube.com/channel/UCe-j7AfjiJFsSPyvlzsmVMQ" class="social_icon" id="youtube" target="blank">Youtube</a>
+									</div>
+                    <!-- <img src="<?php// echo $tdu; ?>/img/ensr_summer.jpg" alt=""> -->
+                </div>
+
+
+            </div>
+
+
+        </div>
+
+        <div id="copyright">
+            <p class="container">&copy; <?php echo date('Y'); ?> Ecole Nouvelle de la Suisse Romande.  Website by <a href="https://webfactor.ch">Webfactor</a>. </p>
+        </div>
+
+    </footer>
+
+
+        </main>
+
+
+
+		<?php wp_footer(); ?>
+
+		<script type="text/javascript" src="<?php echo $tdu; ?>/js/min/scripts.bundle.js?v=<?php echo wf_version(); ?>"></script>
+		<script>
+		// (function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
+		// (f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
+		// l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
+		// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		// ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+		// ga('send', 'pageview');
+		</script>
+
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+        <?php $tdu =  get_template_directory_uri(); ?>
+        <link href="//www.google-analytics.com" rel="dns-prefetch">
+
+        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $tdu; ?>/img/favicon/apple-touch-icon.png?v=2">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $tdu; ?>/img/favicon/favicon-32x32.png?v=2">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $tdu; ?>/img/favicon/favicon-16x16.png?v=2">
+        <link rel="manifest" href="<?php echo $tdu; ?>/img/favicon/manifest.json?v=2">
+        <link rel="mask-icon" href="<?php echo $tdu; ?>/img/favicon/safari-pinned-tab.svg?v=2" color="#005ca9">
+        <link rel="shortcut icon" href="/favicon.ico?v=2">
+        <meta name="apple-mobile-web-app-title" content="ENSR Summer Camp">
+        <meta name="application-name" content="ENSR Summer Camp">
+        <meta name="theme-color" content="#ffffff">
+
+        <link rel="stylesheet" type="text/css" href="<?php echo $tdu; ?>/MyFontsWebfontsKit.css">
+        <link href="https://fonts.googleapis.com/css?family=Baloo+Tammudu" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lobster+Two:700i" rel="stylesheet">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="<?php bloginfo('description'); ?>">
+
+        <?php wp_head(); ?>
+
+
+    </head>
+    <body <?php body_class(); ?>>
+
+
+        <header class="header" id="header">
+            <nav>
+
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-3 col-sm-push-0 col-xs-10 col-xs-push-1">
+                            <ul>
+                                <li> <a href="<?php echo home_url(); ?>" class="branding"><?php bloginfo('name'); ?></a></li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-9">
+                            <div id="navigation_menu">
+                                <ul>
+                                    <?php chilly_nav('primary-navigation'); ?>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
+                    <a href="#" id="menu_button" >Menu</a>
+                </div>
+
+            </nav>
+        </header>
+
+        <main id="main" >
