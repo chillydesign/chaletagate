29324fc267858f2f6eef7b20ee53cc99ec1ec6a5
Charles Harvey <harvey.charles@gmail.com>
2018-02-22 12:20:10 +0100

    dont allow svg uploads any more

--- functions.php
+++ functions.php
@@ -576,21 +576,16 @@ function thumbnail_of_post_url( $post_id,  $size='large'  ) {
 
 }
 
-
-
-function cc_mime_types($mimes) {
-  $mimes['svg'] = 'image/svg+xml';
-  return $mimes;
-}
-add_filter('upload_mimes', 'cc_mime_types');
-
-
-
-
-
-
-
-include('functions_form.php');
+// 
+//
+// function cc_mime_types($mimes) {
+//   $mimes['svg'] = 'image/svg+xml';
+//   return $mimes;
+// }
+// add_filter('upload_mimes', 'cc_mime_types');
+//
+//
+//
 
 
 
